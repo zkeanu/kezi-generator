7fc178c088bdcf8d0904ef18f3a54415b85c5c41
keanu <3352103646@qq.com>
2025-02-27 16:59:16 +0800

    项目工程初始化

---
+++ kezi-generator-basic/src/main/java/com/keanu/Main.java
@@ -0,0 +1,25 @@
+package com.keanu;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author <a href="https://github.com/Li-Keanu">keanu</a>
+ * @since 2025/2/27 16:43
+ */
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
